42cd543364fedca6ee7917979bf70ea1f0bd9651
Kip Cole <kipcole9@gmail.com>
2019-08-20 20:16:49 +0800

    Move Money.Migration to ex_money_sql

--- lib/money_sql.ex
+++ lib/money_sql.ex
@@ -1,18 +1,4 @@
-defmodule MoneySql do
-  @moduledoc """
-  Documentation for MoneySql.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MoneySql.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+defmodule Money.Sql do
+  @moduledoc false
+  
 end
