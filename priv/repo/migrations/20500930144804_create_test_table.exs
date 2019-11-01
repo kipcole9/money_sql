c23e7e3abebd7e9012bc6988f4111f415c60761f
Kip Cole <kipcole9@gmail.com>
2019-11-02 06:43:41 +0800

    Add back test table

---
+++ priv/repo/migrations/20500930144804_create_test_table.exs
@@ -0,0 +1,12 @@
+defmodule Money.Repo.Migrations.CreateMoneyTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:organizations) do
+      add :name,            :string
+      add :employee_count,  :integer
+      add :payroll,         :money_with_currency
+      timestamps()
+    end
+  end
+end
