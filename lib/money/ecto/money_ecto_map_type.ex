33dbf82b8505c0b566f2d1d7739a949420eabe28
Kip Cole <kipcole9@gmail.com>
2022-12-18 03:02:40 +1100

    Don't localized parse loading json type

--- lib/money/ecto/money_ecto_map_type.ex
+++ lib/money/ecto/money_ecto_map_type.ex
@@ -38,7 +38,7 @@ if Code.ensure_loaded?(Ecto.Type) do
 
     def load(%{"currency" => currency, "amount" => amount}, _loader, params)
         when is_binary(amount) do
-      with {amount, ""} <- Cldr.Decimal.parse(amount),
+      with {amount, ""} <- Decimal.parse(amount),
            {:ok, currency} <- Money.validate_currency(currency) do
         {:ok, Money.new(currency, amount, params)}
       else
